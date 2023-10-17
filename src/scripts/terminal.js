21b912bbb78a118a21b4cef4deef85b4d308680a
Samotage820 <samjirik@gmail.com>
2023-10-17 02:20:38 -0500

    added history

--- src/scripts/terminal.js
+++ src/scripts/terminal.js
@@ -2,6 +2,8 @@ function Terminal() {
     let cmdLine;
     let output;
     let prompt;
+    let history = [];
+    let historyPos = 0;
     let terminalHeight = 10;
 
     function init() {
@@ -11,17 +13,43 @@ function Terminal() {
 
         setPrompt("[user@host] > ");
         cmdLine.addEventListener("keydown", processCommand);
+        cmdLine.addEventListener("keydown", navigateHistory);
     }
 
     function processCommand(e) {
-        if (e.keyCode == 13 && this.value && this.value.trim) {
+        if (e.keyCode == 13 && this.value && this.value.trim()) {
             let cmd = this.value.trim();
             this.value = "";
+
+            if (history.length == 0 || history[history.length - 1] != cmd) {
+                history[history.length] = cmd;
+                historyPos = history.length;
+            }
 
             output.insertAdjacentHTML("beforeEnd", "<p>" + cmd + "</p>");
             if (output.children.length > terminalHeight) {
                 output.children[0].remove();
             }
+            console.log(history);
+        }
+    }
+
+    function navigateHistory(e) {
+        if (history.length && (e.keyCode == 38 || e.keyCode == 40)) {
+            if (e.keyCode == 38) {
+                historyPos--;
+                if (historyPos < 0) {
+                    historyPos = 0;
+                }
+            }
+            else if (e.keyCode == 40) {
+                historyPos++;
+                if (historyPos >= history.length) {
+                    historyPos = history.length - 1;
+                }
+            }
+
+            this.value = history[historyPos];
         }
     }
 
